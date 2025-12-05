9a10d2534bba358714296b000d19eda7b3d3890c
dewirahayu-cmd <d3wi.r4hayu@gmail.com>
2025-12-05 09:18:02 +0700

    modif 1

--- pengunjungCafe08.java
+++ pengunjungCafe08.java
@@ -1,16 +1,17 @@
 public class pengunjungCafe08 {
     
-    public static void daftarPengunjung(String... namaPengunjung) {
-        System.out.println("===== DAFTAR PENGUNJUNG KAFE =====");
-        System.out.println("Jumlah pengunjung: " + namaPengunjung.length);
-        System.out.println("\nDaftar nama pengunjung:");
-        
-        for (int i = 0; i < namaPengunjung.length; i++) {
-            System.out.println((i + 1) + ". " + namaPengunjung[i]);
-        }
-        System.out.println("==================================\n");
-    }
+  public static void daftarPengunjung(String... namaPengunjung) {
+    System.out.println("===== DAFTAR PENGUNJUNG KAFE =====");
+    System.out.println("Jumlah pengunjung: " + namaPengunjung.length);
+    System.out.println("\nDaftar nama pengunjung:");
     
+    int nomor = 1;
+    for (String nama : namaPengunjung) {
+        System.out.println(nomor + ". " + nama);
+        nomor++;
+    }
+    System.out.println("==================================\n");
+}
     public static void main(String[] args) {
         daftarPengunjung("Ali", "Budi", "Citra");
         daftarPengunjung("Ali", "Budi", "Citra", "Doni", "Eka");
